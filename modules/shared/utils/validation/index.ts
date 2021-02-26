b5707006e3336009a0f4d05d5e707702391a533b
Neznayka <bugayow.andrei2014@gmail.com>
2021-02-26 15:55:06 +0200

    'add-custom-hooks-useValidetion-and-useInput'

--- modules/shared/utils/validation/index.ts
+++ modules/shared/utils/validation/index.ts
@@ -1,61 +1,123 @@
+//hook(s)
 import React, { useEffect, useState } from 'react';
 
-export const useValidations = (value: string, validations: any) => {
-  const [isEmpty,setEmpty] = useState(true);
-  const [minLengthError,setMinLengthError] = useState(true);
-  const [maxLengthError,setMaxLengthError] = useState(true);
-  const [emailError,setEmailError] = useState(true);
+interface validationsType {
+  isPhone: boolean;
+  isEmpty: boolean;
+  isCapital: boolean;
+  isSymbol: boolean;
+  isEmail: boolean;
+  maxLength: number;
+  minLength: number;
+}
 
+export const useValidations = (value: string, validations: validationsType) => {
+  //local states
+  const [isEmpty, setEmpty] = useState(false);
+  const [minLengthError, setMinLengthError] = useState(false);
+  const [maxLengthError, setMaxLengthError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [phoneError, setPhoneError] = useState(false);
+  const [capitalError, setCapitalError] = useState(false);
+  const [symbolError, setSymbolError] = useState(false);
+  const [inputValid, setInputValid] = useState(true);
+
+  //Effect(s)
   useEffect(() => {
+    //make іterates over an object validations
     for (const validation in validations) {
-      switch (validation){
-
+      // checks the keys of the elements of an object
+      switch (validation) {
+        //If your input needed min length value
         case 'minLength':
-          value.length < validations[validation] ? setMinLengthError(true) : setMinLengthError(false)
+          value.length < validations[validation] ? setMinLengthError(true) : setMinLengthError(false);
           break;
 
+        //If your input needed max length value
         case 'maxLength':
-          value.length > validations[validation] ? setMaxLengthError(true) : setMaxLengthError(false)
+          value.length > validations[validation] ? setMaxLengthError(true) : setMaxLengthError(false);
           break;
 
-        case 'isEmpty':
-          value ? setEmpty(false) : setEmpty(true)
+        //If your input needed capital value
+        case 'isCapital':
+          value.split('').some((e) => 'A' <= e && e <= 'Z') ? setCapitalError(false) : setCapitalError(true);
           break;
+
+        //If your input needed symbol value
+        case 'isSymbol':
+          value.split('').some((e) => /[-!$%^&*()_+|~=`{}[\]:";'<>?,./]/.test(e))
+            ? setSymbolError(false)
+            : setSymbolError(true);
+          break;
+
+        /*TODO*/
+        case 'isEmpty':
+          value ? setEmpty(false) : setEmpty(true);
+          break;
+
+        // If your input need phone type
+        case 'isPhone':
+          // eslint-disable-next-line no-case-declarations
+          const phoneRegular = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s./0-9]*$/g;
+          phoneRegular.test(String(value)) ? setPhoneError(false) : setPhoneError(true);
+          break;
+
+        //If your input need email type
         case 'isEmail':
           // eslint-disable-next-line no-case-declarations
-          const regular = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          regular.test(String(value).toLowerCase()) ? setEmailError(false) : setEmailError(true)
+          const emailRegular = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          emailRegular.test(String(value).toLowerCase()) ? setEmailError(false) : setEmailError(true);
+          break;
       }
     }
-  },[value])
+  }, [value]);
+
+  useEffect(() => {
+    if (isEmpty || minLengthError || maxLengthError || symbolError || capitalError || emailError || phoneError) {
+      setInputValid(false);
+    } else {
+      setInputValid(true);
+    }
+  }, [isEmpty, minLengthError, symbolError, capitalError, maxLengthError, emailError, phoneError]);
+
+  const checkIsEmpty = () => {
+    value.length > 0 ? setEmpty(false) : setEmpty(true);
+  };
+
   return {
     isEmpty,
     minLengthError,
     maxLengthError,
     emailError,
-  }
-}
-
+    phoneError,
+    capitalError,
+    symbolError,
+    inputValid,
+    checkIsEmpty
+  };
+};
 
 export const useInput = (initialValue: string, validations: any) => {
-  const [value,setValue] = useState(initialValue)
-  const [isDirty, setDirty] = useState(false)
-  const valid = useValidations(value, validations)
+  const [value, setValue] = useState(initialValue);
+  const [isDirty, setDirty] = useState(false);
+  const valid = useValidations(value, validations);
 
-
+  //func if input value changes
   const onChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setValue(event.target.value)
-  }
+    setValue(event.target.value);
+  };
 
-  const onBlur = (event: React.FocusEvent<HTMLInputElement>): void => {
-    setDirty(true)
-  }
+  //func if input on blur
+  const onBlur = (): void => {
+    setDirty(true);
+    valid.checkIsEmpty();
+  };
 
-  return{
+  return {
     value,
     onChange,
     onBlur,
     isDirty,
     ...valid
-  }
-}
+  };
+};
