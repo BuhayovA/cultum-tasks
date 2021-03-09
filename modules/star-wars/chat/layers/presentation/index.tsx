0f7bfc2260a0431f4fdb97d870c853dbaab873be
Neznayka <bugayow.andrei2014@gmail.com>
2021-03-09 18:51:36 +0200

    'fix-all-bugs'

--- modules/star-wars/chat/layers/presentation/index.tsx
+++ modules/star-wars/chat/layers/presentation/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext } from 'react';
 //views
 import { Wrapper, InputWrapper, MessageWrapper, ChatWrapper, SwapPersonWrapper } from './views';
 import ChatInput from '@md-star-wars/chat/components/chat-input';
